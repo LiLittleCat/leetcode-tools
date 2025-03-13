59d7af3c578c10fabe6c490c422ccdd1a89ba6bc
Yi Liu <lilittlecat@outlook.com>
2025-03-13 22:04:40 +0800

    Remove GitHub link and associated styles from titleSlug extractor UI

--- leetcode_title_slug_extractor.user.js
+++ leetcode_title_slug_extractor.user.js
@@ -60,30 +60,6 @@
             margin: 0;
         }
 
-        #leetcode-title-slug-extractor-github {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            padding: 4px;
-            background: #24292e;
-            color: #fff;
-            border-radius: 4px;
-            text-decoration: none;
-            transition: background 0.2s;
-            width: 24px;
-            height: 24px;
-        }
-
-        #leetcode-title-slug-extractor-github:hover {
-            background: #1a1f23;
-        }
-
-        #leetcode-title-slug-extractor-github svg {
-            width: 16px;
-            height: 16px;
-            fill: currentColor;
-        }
-
         #leetcode-title-slug-extractor-toggle {
             background: none;
             border: none;
@@ -201,11 +177,6 @@
         <div id="leetcode-title-slug-extractor-header">
             <div id="leetcode-title-slug-extractor-title-section">
                 <h3 id="leetcode-title-slug-extractor-title">LeetCode 题目 titleSlug 提取器</h3>
-                <a href="https://github.com/LiLittleCat/leetcode-tools" target="_blank" id="leetcode-title-slug-extractor-github">
-                    <svg viewBox="0 0 16 16">
-                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/>
-                    </svg>
-                </a>
             </div>
             <button id="leetcode-title-slug-extractor-toggle">−</button>
         </div>
